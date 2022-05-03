72899f9483d69d237a7b09086a1b514712d23e5b
ElmerPC <erey2790@gmail.com>
2022-05-03 16:27:49 -0500

    finished with navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,21 @@ import React, { Component } from 'react'
 import Navitem from './Navitem'
 
 class Navbar extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            'NavItemActive':''
+        }
+    }
+
+    activeItem = (x) => {
+        if (this.state.NavItemActive.length > 0) {
+            document.getElementById(this.state.NavItemActive).classList.remove('active');
+        }
+        this.setState({ 'NavItemId': x}, () =>{
+            document.getElementById(this.state.NavItemActive).classList.add('active');
+        });
+    };
     render() {
         return (
             <nav>
