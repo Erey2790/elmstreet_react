b756ace9d543a3ed9aaa08d404e6c82e1ec96669
ElmerPC <erey2790@gmail.com>
2022-05-07 21:48:47 -0500

    added socials

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons'
 
 class Social extends Component {
     render() {
         return (
             <div className='social'>
-                <a href="https://github.com/erey2790" target="_blank"><i class="fab fa-github"></i></a>
-                <a href="https://Linkedin.com/in/elmer-reyes-56091221b/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+              <FontAwesomeIcon icon={faGithub} size="4x"></FontAwesomeIcon>
+              <FontAwesomeIcon icon={faTwitter} size="4x"></FontAwesomeIcon>
             </div>
         )
     }
