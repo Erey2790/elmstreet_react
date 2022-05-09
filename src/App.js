c6ce6a408b6223c4c0fc52da5521b31a91964463
ElmerPC <erey2790@gmail.com>
2022-05-09 00:20:33 -0500

    about section now works

--- src/App.js
+++ src/App.js
@@ -1,41 +1,32 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './contents/Home';
 import About from './contents/About';
-
 // import Education from './contents/Education';
 // import Skills from './contents/Skills';
 // import Contact from './contents/Contact';
- 
-
-
 function App() {
-  return (
-    <Router>
-    <div className="App">
-    <Navbar />
-    <Switch>
-    <Route exact path="/">
-    <Home />
-    </Route>
-    <Route path="/about">
-    <About />
-    </Route>
-    </Switch>
-    {/* <Route path="/education">
-    <Education />
-    </Route>
-    <Route path="/skills">
-    <Skills />
-    </Route>
-    <Route path="/contact">
-    <Contact />
-    </Route> */}
-    </div>
-    </Router>
+ return (
+        <Router>
+            <div className='App'>
+                <Navbar />
+                <Routes>
+                    <Route exact path='/' element={<Home />} />
+                    <Route path='/about' element={<About />} />
+                </Routes>
+                {/* <Route path="/education">
+   <Education />
+   </Route>
+   <Route path="/skills">
+   <Skills />
+   </Route>
+   <Route path="/contact">
+   <Contact />
+   </Route> */}
+            </div>
+        </Router>
     );
-  }
-  
-  export default App;
+ }
+ export default App;
