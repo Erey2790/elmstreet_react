3a0ce08fe84ab26550b2653bb233db9f593a5362
ElmerPC <erey2790@gmail.com>
2022-05-08 23:14:00 -0500

    fixed the links at the bottom for social

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faGithub, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons'
 
 class Social extends Component {
     render() {
         return (
             <div className='social'>
                 <a href="https://github.com/Erey2790">
-              <FontAwesomeIcon icon={faGithub} size="4x" color='#24292f'></FontAwesomeIcon></a>
+              <FontAwesomeIcon icon={faGithub} size="4x" color='#24292f' className='github'></FontAwesomeIcon></a>
               <a href="https://github.com/Erey2790">
-              <FontAwesomeIcon icon={faTwitter} size="4x" color='#0099CC'></FontAwesomeIcon></a>
+              <FontAwesomeIcon icon={faTwitter} size="4x" color='#0099CC' className='twitter'></FontAwesomeIcon></a>
+              <a href="https://www.linkedin.com/in/elmer-reyes-56091221b/">
+              <FontAwesomeIcon icon={faLinkedin} size="4x" color='#0072b1' className='linkedIn'></FontAwesomeIcon></a>
             </div>
         )
     }
