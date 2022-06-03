6a264c2eedbdb7952668c6b2c25ff4e77a2ff9d6
ElmerPC <erey2790@gmail.com>
2022-06-02 22:25:30 -0500

    added href to projects

--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -10,6 +10,8 @@ import shopshop from '../img/shopshop.PNG';
 
 
 
+
+
 class ProjectCards extends Component {
     render() {
         return(
@@ -17,21 +19,21 @@ class ProjectCards extends Component {
                 
                     <div className='container'>
                     <img src={shopshop} alt='project 1' className='projectPic'/>
-                        <h4><b>Shop Shop</b></h4>
+                        <h4><a href='https://github.com/Erey2790' className='projectLinks'><b>Shop Shop</b></a></h4>
                         <p>This project uses MongoDB and Imsonmia to showcase the fundamentals of the CRUD routes.</p>
                     </div>
 
                     
                     <div className='container'>
                     <img src={budgetTracker}  alt='project 2' className='projectPic'/>
-                        <h4><b>Budget Tracker</b></h4>
+                        <h4><a href='https://github.com/Erey2790' className='projectLinks'><b>Budget Tracker</b></a></h4>
                         <p>This application utilizes Service Workers, Web Manifest, IndexedDB, and is deployed with Heroku.</p>
                     </div>
 
                     
                     <div className='container'>
                     <img src={drinkmoto} alt='project 3' className='projectPic'/>
-                        <h4><b>Cocktail Roulette</b></h4>
+                        <h4><a href='https://github.com/Erey2790' className='projectLinks'><b>Cocktail Roulette</b></a></h4>
                         <p>A web application that uses JAVASCRIPT and a third party API to generate a random drink.</p>
                     </div>
             </div>
